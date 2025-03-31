93def5feced580f6bdfd081577241cbd39ed2137
Abhishek Srikanth <abhisrik99@outlook.com>
2025-03-31 17:37:18 -0400

    feat: Add CORS settings to Django configuration

--- saturchat/settings.py
+++ saturchat/settings.py
@@ -171,3 +171,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
+# CORS settings
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 
+                             'http://localhost:3000,http://127.0.0.1:3000').split(',')
