2e6207b3c26c10ef4e9626dcd4bb38c2f900165f
Abhishek Srikanth <abhisrik99@outlook.com>
2025-03-31 17:35:32 -0400

    feat: Add Celery configuration to Django settings

--- saturchat/settings.py
+++ saturchat/settings.py
@@ -112,6 +112,10 @@ CHANNEL_LAYERS = {
     },
 }
 
+# Celery settings
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('CELERY_BACKEND', 'redis://localhost:6379/0')
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
