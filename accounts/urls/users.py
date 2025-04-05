fb8f01c538c34bb554b6ce78dbdc54cd416b49f2
Abhishek Srikanth <abhisrik99@outlook.com>
2025-04-05 12:30:51 -0400

    fix: add urlpatterns to include user router for improved URL handling

--- accounts/urls/users.py
+++ accounts/urls/users.py
@@ -1,6 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from accounts.views import UserViewSet
+from django.urls import path, include
 
 
 router = DefaultRouter()
 router.register(r'', UserViewSet, basename='user')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
