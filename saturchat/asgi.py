f83cb27aeee7c84191164511dfd8b6ec818c3d95
Abhishek Srikanth <abhisrik99@outlook.com>
2025-04-05 19:10:47 -0400

    fix: reorder import statements in ASGI configuration

--- saturchat/asgi.py
+++ saturchat/asgi.py
@@ -4,10 +4,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 
-from chat.routing import websocket_urlpatterns
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saturchat.settings')
 django.setup()
+
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
