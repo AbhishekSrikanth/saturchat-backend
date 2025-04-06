a2d1201af765a69adf6f95b2918fb11e5b0c94b4
Abhishek Srikanth <abhisrik99@outlook.com>
2025-04-05 20:53:19 -0400

    feat: replace AuthMiddlewareStack with JWTAuthMiddleware for WebSocket authentication

--- saturchat/asgi.py
+++ saturchat/asgi.py
@@ -2,16 +2,16 @@ import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saturchat.settings')
 django.setup()
 
 from chat.routing import websocket_urlpatterns
+from accounts.middleware import JWTAuthMiddleware
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "websocket": JWTAuthMiddleware(
         URLRouter(websocket_urlpatterns)
     ),
 })
